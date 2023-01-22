4bb71fe8db464504687ff69be074d36c6ae90e20
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 <yasen.angelov@news.co.uk>
2023-01-22 22:51:21 +0200

    feat: Moving all bootstrap functions to new directory. feat: New dependencies included in Container chore: removing unused dependencies form Container

---
+++ src/Bootstrap/dependencies.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Helpers\ControllersFactory;
+use App\Helpers\InputValidator;
+use App\Interfaces\Validator;
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Translation\ArrayLoader;
+use Illuminate\Translation\Translator;
+use Illuminate\Validation\Factory;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
+
+if (isset($app)) {
+    $container = $app->getContainer();
+}
+
+$container['logger'] = function ($c) {
+    $settings = $c->get('settings')['logger'];
+    $logger = new Monolog\Logger($settings['name']);
+    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
+    return $logger;
+};
+
+$container['view'] = function ($container): Twig {
+    $view = new Twig( dirname(__DIR__, 2) . '/resources/view/templates',
+    [
+        'cache' => false
+    ]);
+
+    $view->addExtension(new TwigExtension(
+        $container->router,
+        $container->request->getUri()
+    ));
+
+    return $view;
+};
+
+$container['db'] = function ($container) {
+    $capsule = new Manager;
+    $capsule->addConnection($container['settings']['db']);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+};
+
+$container['validator'] = function (): Validator {
+    return new InputValidator();
+};
+
+// Inject controllers callables into the container
+$controllers = ControllersFactory::controllers_lazy_loader($container->get('view'), $container->get('logger'), $container->get('validator'));
+foreach ($controllers as $controller_tag => $controller_callback) {
+    $container[$controller_tag] = $controller_callback;
+}
