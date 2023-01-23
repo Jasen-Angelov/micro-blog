531009b2462d70978fd829b3641a613c426f1f1d
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 <yasen.angelov@news.co.uk>
2023-01-23 11:21:21 +0200

    chore: routes reorganisation

--- src/Bootstrap/routes.php
+++ src/Bootstrap/routes.php
@@ -4,19 +4,15 @@
 use App\Middleware\Admin              as AdminMiddleware;
 use App\Middleware\Editor             as EditorMiddleware;
 use App\Middleware\UserAuthentication as AuthenticationMiddleware;
-use Slim\Http\Request;
-use Slim\Http\Response;
+
 
 if (isset($app)) {
+    $app->get( '/',            'blogs.controller')->setName('home.blogs');
+    $app->get( '/blog/{slug}', 'blogs.controller')->setName('home.blog');
+    $app->any('/login',        'login.controller')->setName('admin.login');
+    $app->any('/register',     'rgstr.controller')->setName('admin.register');
 
-    $app->get( '/', function (Request $request, Response $response, array $args) {
-       return $response->withRedirect('/blog');
-    })->setName('home');
-
-    $app->get( '/blog', 'home.controller')->setName('home.blogs');
-    $app->any('/login',          'login.controller')->setName('home.login');
-    $app->any('/register',       'registration.controller')->setName('home.register');
-
+    // Administration routes
     $app->group('/admin', function () use ($app) {
         $this->get('/',             'admin.controller')->setName('admin.panel');
         $this->any('/posts/[{id}]', 'post.controller')->setName('admin.posts');
