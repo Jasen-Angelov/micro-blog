313683975a7d5c05d815c75537864083312d720f
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 <yasen.angelov@news.co.uk>
2023-01-26 09:03:54 +0200

    feat: New Blog Manager for upserting blogs.

---
+++ src/Services/BlogManager.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Blog;
+use Illuminate\Database\Eloquent\Model;
+use Monolog\Logger;
+
+class BlogManager
+{
+    public static function upsert_blog(iterable $data, iterable $resource = [], Blog $blog = null ):bool
+    {
+        $blog = $blog ?? new Blog();
+        foreach ($data as $key => $val){
+            $blog->$key = $val;
+        }
+        if (empty($blog->slug)){
+            $blog->slug = self::create_unique_slug($blog->title);
+        }
+        foreach ($resource as $name => $value){
+           if ($value instanceof Model && method_exists($blog, $name)){
+              $blog->$name->save($value);
+           }
+        }
+
+        return $blog->save();
+    }
+
+    public static function create_unique_slug(string $text, string $divider = '-'):string
+    {
+        // replace non letter or digits by divider
+        $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, $divider);
+
+        // remove duplicate divider
+        $text = preg_replace('~-+~', $divider, $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        $text = $text ?? 'n-a';
+
+        $existing_slugs = Blog::where('slug', 'like', $text . '%')->count();
+
+        if ($existing_slugs > 0){
+            $text .= '-' . $existing_slugs + 1;
+        }
+
+        return $text;
+    }
+
+}
