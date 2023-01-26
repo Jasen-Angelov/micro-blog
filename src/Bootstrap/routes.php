085df282335df199c0dd3977a18b52e8c5a6785e
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 <yasen.angelov@news.co.uk>
2023-01-26 08:49:40 +0200

    feat: New route groups and routes

--- src/Bootstrap/routes.php
+++ src/Bootstrap/routes.php
@@ -1,24 +1,22 @@
 <?php
-// Routes
 
-use App\Middleware\Admin              as AdminMiddleware;
-use App\Middleware\Editor             as EditorMiddleware;
+use App\Middleware\BlogValidator;
 use App\Middleware\UserAuthentication as AuthenticationMiddleware;
 
-
 if (isset($app)) {
-    $app->get( '/',            'blogs.controller')->setName('home.blogs');
-    $app->get( '/blog/{slug}', 'blogs.controller')->setName('home.blog');
+    $app->get( '/',            'blog.controller')->setName('home.blogs');
+    $app->get( '/blog/{slug}', 'blog.controller')->setName('home.blog');
     $app->any('/login',        'login.controller')->setName('admin.login');
     $app->any('/register',     'rgstr.controller')->setName('admin.register');
 
     // Administration routes
     $app->group('/admin', function () use ($app) {
-        $this->get('/',             'admin.controller')->setName('admin.panel');
-        $this->any('/posts/[{id}]', 'post.controller')->setName('admin.posts');
-
-        $this->group('/users', function () use ($app) {
-            $this->any('/[{id}]', 'user.controller')->add(AdminMiddleware::class)->setName('admin.users');
-        });
-    })->add(AuthenticationMiddleware::class)->add(EditorMiddleware::class);
+        $this->get('',                     'admin.controller')->setName('admin.dashboard');
+        $this->get('/dashboard',           'admin.controller')->setName('admin.dashboard');
+        $this->get('/blog/[{id}]',         'admin.blog.controller')->setName('admin.blog.get');
+        $this->put('/blog/[{id}]',         'admin.blog.controller')->setName('admin.blog.update');
+        $this->post('/blog/[{id}]',        'admin.blog.controller')->setName('admin.blog.create');
+        $this->get('/blog/delete/{id}',    'admin.blog.controller:delete')->setName('admin.blog.delete');
+        $this->get('/logout',              'login.controller:delete')->setName('admin.logout');
+    })->add(AuthenticationMiddleware::class)->add(BlogValidator::class);
 }
