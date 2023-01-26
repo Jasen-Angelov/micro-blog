9a1dbead48c618dff6757818c6575b10fd86d998
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 <yasen.angelov@news.co.uk>
2023-01-26 08:47:16 +0200

    feat: JS and CSS, as well as few modification on the index.php

--- public/index.php
+++ public/index.php
@@ -1,32 +1,33 @@
 <?php
 
 use Slim\App;
+define('APP_ROOT', dirname(__DIR__));
 
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
-    $file = __DIR__ . $_SERVER['REQUEST_URI'];
+    $file = APP_ROOT . $_SERVER['REQUEST_URI'];
     if (is_file($file)) {
         return false;
     }
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+require APP_ROOT . '/vendor/autoload.php';
 
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require APP_ROOT . '/src/Bootstrap/settings.php';
 $app = new App($settings);
 
 // Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
+require APP_ROOT . '/src/Bootstrap/dependencies.php';
 
 // Register middleware
-require __DIR__ . '/../src/middleware.php';
+require APP_ROOT . '/src/Bootstrap/middleware.php';
 
 // Register routes
-require __DIR__ . '/../src/routes.php';
+require APP_ROOT . '/src/Bootstrap/routes.php';
 
 // Run app
 $app->run();
