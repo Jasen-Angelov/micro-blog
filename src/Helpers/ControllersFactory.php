2091a493a5cbb4f67771ac921e81120bcecf584f
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 <yasen.angelov@news.co.uk>
2023-01-26 08:57:12 +0200

    feat: New ControllersFactory that will inject dependencies and return callable function with the controller inside.

--- src/Helpers/ControllersFactory.php
+++ src/Helpers/ControllersFactory.php
@@ -4,35 +4,34 @@ namespace App\Helpers;
 
 use App\Controllers\Admin\Admin;
 use App\Controllers\Admin\LogIn;
-use App\Controllers\Admin\Logout;
-use App\Controllers\Admin\Post;
+use App\Controllers\Admin\Blog as BlogEdit;
 use App\Controllers\Admin\Registration;
 use App\Controllers\Admin\User;
 use App\Controllers\BaseController;
-use App\Controllers\Public\Blogs;
+use App\Controllers\Public\Blog;
 use App\Interfaces\Validator;
 use Monolog\Logger;
+use Slim\Flash\Messages;
 use Slim\Views\Twig;
 
 class ControllersFactory
 {
     private static array $controllers = [
-        'blogs.controller'   => Blogs::class,
-        'login.controller'   => LogIn::class,
-        'rgstr.controller'   => Registration::class,
-        'logout.controller'  => Logout::class,
-        'admin.controller'   => Admin::class,
-        'post.controller'    => Post::class,
-        'user.controller'    => User::class
+        'blog.controller'       => Blog::class,
+        'login.controller'      => LogIn::class,
+        'rgstr.controller'      => Registration::class,
+        'admin.controller'      => Admin::class,
+        'admin.blog.controller' => BlogEdit::class,
+        'user.controller'       => User::class
     ];
 
-    public static function controllers_lazy_loader(Twig $view, Logger $logger, Validator $validator): array
+    public static function controllers_lazy_loader(Twig $view, Logger $logger, Validator $validator, Messages $flash_message): array
     {
         $controllers = [];
         foreach (self::$controllers as $controller_tag => $controller_class) {
-            $controllers[$controller_tag] = function () use ($view, $logger, $controller_class, $validator): BaseController {
+            $controllers[$controller_tag] = function () use ($view, $logger, $controller_class, $validator, $flash_message): BaseController {
 
-                return new $controller_class($view, $logger, $validator);
+                return new $controller_class($view, $logger, $validator, $flash_message);
             };
         }
 
