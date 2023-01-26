5377c797df9acd0655b13e9f3ba79f2a83897462
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 <yasen.angelov@news.co.uk>
2023-01-26 08:57:52 +0200

    feat: New Image manager that handles image file uploads

---
+++ src/Helpers/ImageManager.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\Image;
+use Slim\Http\UploadedFile;
+
+class ImageManager
+{
+   private static string $directory = DIRECTORY_SEPARATOR .'resources/images' . DIRECTORY_SEPARATOR;
+
+   public static function create_image_from_file(UploadedFile $data): Image
+   {
+       $path = self::move_uploaded_file($data);
+       $image = new Image();
+       $image->path = $path;
+       $image->name = htmlentities($data->getClientFilename());
+       $image->save();
+
+       return $image;
+   }
+   private static function move_uploaded_file( UploadedFile $uploadedFile): string
+   {
+        $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+        $basename = bin2hex(random_bytes(8));
+        $filename = sprintf('%s.%0.8s', $basename, $extension);
+        $uploadedFile->moveTo(APP_ROOT . self::$directory . $filename);
+
+        return self::$directory . $filename;
+   }
+
+    public static function delete_existing_images( Image $image): bool
+    {
+        if (file_exists($image->path)){
+
+            return unlink($image->path);
+        }
+
+        return $image->delete();
+    }
+}
