71609009967de628ba6cd3cdb9032b8331a5c267
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 <yasen.angelov@news.co.uk>
2023-01-29 16:41:46 +0200

    feat: Adding 404 page and streamline the logic in the public controller

--- src/Controllers/Public/Blog.php
+++ src/Controllers/Public/Blog.php
@@ -14,23 +14,22 @@ class Blog extends BaseController
      */
     public function get(Request $request, Response $response, array $args = []): Response
     {
-        if (isset($args['slug'])) {
-            $this->validator->name('slug')->value($args['slug'])->pattern('slug')->is_required();
-            if ($this->validator->has_errors()) {
-                $this->logger->error('Invalid slug', $this->validator->get_errors());
+        if (false === $request->getAttribute('validation_success')){
+            return $this->view->render($response, '/pages/404.twig');
+        }
 
-                return $this->view->render($response, '404.twig');
-            }
-            $post = BlogPost::where(['slug' => $args['slug']])->first();
+        $slug = $args['slug'] ?? null;
+        if ($slug) {
+            $post = BlogPost::where(['slug' => $slug])->first();
             if ($post) {
                 return $this->view->render($response, '/pages/public/blog.twig', ['blog' => $post]);
             } else {
-                return $this->view->render($response, '404.twig');
+                return $this->view->render($response, '/pages/404.twig');
             }
-        } else {
-            $posts = BlogPost::orderBy('created_at', 'desc')->get();
-            return  $this->view->render($response, '/pages/public/blog-list.twig', ['blogs' => $posts]);
         }
+        $posts = BlogPost::orderBy('created_at', 'desc')->get();
+
+        return  $this->view->render($response, '/pages/public/blog-list.twig', ['blogs' => $posts]);
     }
 
     /**
@@ -38,7 +37,7 @@ class Blog extends BaseController
      */
     public function post(Request $request, Response $response, array $args = []): Response
     {
-        return $response->withStatus(405);
+        return $this->view->render($response, '/pages/404.twig');
     }
 
     /**
@@ -46,7 +45,7 @@ class Blog extends BaseController
      */
     public function put(Request $request, Response $response, array $args = []): Response
     {
-        return $response->withStatus(405);
+        return $this->view->render($response, '/pages/404.twig');
     }
 
     /**
@@ -54,6 +53,6 @@ class Blog extends BaseController
      */
     public function delete(Request $request, Response $response, array $args = []): Response
     {
-        return $response->withStatus(405);
+        return $this->view->render($response, '/pages/404.twig');
     }
 }
